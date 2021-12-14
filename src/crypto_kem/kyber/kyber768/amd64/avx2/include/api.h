ea92977e33ef5d8da001dfe32326dbe1fc3edbe6
Peter Schwabe <peter@cryptojedi.org>
2021-12-14 16:30:44 +0100

    kyber768/amd64/avx2 builds.

--- src/crypto_kem/kyber/kyber768/amd64/avx2/include/api.h
+++ src/crypto_kem/kyber/kyber768/amd64/avx2/include/api.h
@@ -1,15 +1,15 @@
-#ifndef CRYPTOLIB_KYBER768_AMD64_AVX2_H
-#define CRYPTOLIB_KYBER768_AMD64_AVX2_H
+#ifndef JADE_KEM_KYBER_KYBER768_AMD64_AVX2_H
+#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_H
 
-#define CRYPTOLIB_KYBER768_SECRETKEYBYTES  2400
-#define CRYPTOLIB_KYBER768_PUBLICKEYBYTES  1184
-#define CRYPTOLIB_KYBER768_CIPHERTEXTBYTES 1088
-#define CRYPTOLIB_KYBER768_BYTES           32
+#define JADE_KEM_KYBER_KYBER768_SECRETKEYBYTES  2400
+#define JADE_KEM_KYBER_KYBER768_PUBLICKEYBYTES  1184
+#define JADE_KEM_KYBER_KYBER768_CIPHERTEXTBYTES 1088
+#define JADE_KEM_KYBER_KYBER768_BYTES           32
 
-int cryptolib_kyber768_amd64_avx2_kem_keypair(uint8_t *pk, uint8_t *sk);
+int jade_kem_kyber_kyber768_amd64_avx2_keypair(uint8_t *pk, uint8_t *sk);
 
-int cryptolib_kyber768_amd64_avx2_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
+int jade_kem_kyber_kyber768_amd64_avx2_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
 
-int cryptolib_kyber768_amd64_avx2_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
+int jade_kem_kyber_kyber768_amd64_avx2_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 
 #endif
