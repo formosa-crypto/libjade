0a8e7e6de07c8089186e66df84d4dfa2d94f41a3
Tiago Oliveira <tfaoliveira@gmail.com>
2022-02-11 19:56:42 +0100

    shake256

---
+++ src/crypto_xof/shake256/amd64/ref/include/api.h
@@ -0,0 +1,17 @@
+#ifndef JADE_XOF_SHAKE256_AMD64_REF_API_H
+#define JADE_XOF_SHAKE256_AMD64_REF_API_H
+
+//TODO REMOVE BYTES
+#define JADE_XOF_SHAKE256_AMD64_REF_BYTES 136
+#define JADE_XOF_SHAKE256_AMD64_REF_ALGNAME "SHAKE256"
+
+#include <stdint.h>
+
+int jade_xof_shake256_amd64_ref(
+ uint8_t *out,
+ uint64_t outlen,
+ uint8_t *in,
+ uint64_t inlen
+);
+
+#endif
