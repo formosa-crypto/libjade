ff31b3b94553a7c7ef86a2fc4e0e13668e306a9e
Tiago Oliveira <tfaoliveira@gmail.com>
2022-02-11 19:13:28 +0100

    shake128

---
+++ src/crypto_xof/shake128/amd64/ref/include/api.h
@@ -0,0 +1,15 @@
+#ifndef JADE_XOF_SHAKE128_AMD64_REF_API_H
+#define JADE_XOF_SHAKE128_AMD64_REF_API_H
+
+#define JADE_XOF_SHAKE128_AMD64_REF_ALGNAME "SHAKE128"
+
+#include <stdint.h>
+
+int jade_xof_shake128_amd64_ref(
+ uint8_t *out,
+ uint64_t outlen,
+ uint8_t *in,
+ uint64_t inlen
+);
+
+#endif
