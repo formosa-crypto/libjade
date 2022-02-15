989cb799349dff52a07aaa3460d064ebb080e442
MQuaresma <miguelquaresma97@gmail.com>
2022-02-15 11:56:36 +0100

    Merge get_random with test0

--- src/crypto_kem/kyber/kyber512/amd64/avx2/include/api.h
+++ src/crypto_kem/kyber/kyber512/amd64/avx2/include/api.h
@@ -9,9 +9,9 @@
 #define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_BYTES           32
 #define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_ALGNAME         "Kyber512"
 
-int jade_kem_kyber_kyber512_amd64_avx2_keypair(uint8_t *pk, uint8_t *sk, const uint8_t *randomness);
+int jade_kem_kyber_kyber512_amd64_avx2_keypair(uint8_t *pk, uint8_t *sk);
 
-int jade_kem_kyber_kyber512_amd64_avx2_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk, const uint8_t *coins);
+int jade_kem_kyber_kyber512_amd64_avx2_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
 
 int jade_kem_kyber_kyber512_amd64_avx2_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 
