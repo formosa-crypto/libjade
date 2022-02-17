15c3e538960a250b6ddd01e19a502e397b8aba12
MQuaresma <miguelquaresma97@gmail.com>
2022-02-17 13:25:38 +0100

    WIP: crypto_rng

---
+++ src/crypto_rng/rand1.c
@@ -0,0 +1,43 @@
+#include <stdint.h>
+
+unsigned char * __jasmin_syscall_randombytes__(unsigned char *x, uint64_t xlen)
+asm("__jasmin_syscall_randombytes__");
+
+#if defined(__linux__) 
+
+#include <unistd.h>
+#include <sys/random.h>
+
+unsigned char * __jasmin_syscall_randombytes__(unsigned char *x, uint64_t xlen)
+{
+
+  int i;
+
+  while (xlen > 0) {
+    if (xlen < 1048576) i = xlen; else i = 1048576;
+
+    i = getrandom(x,i,0);
+    if (i < 1) {
+      sleep(1);
+      continue;
+    }
+    x += i;
+    xlen -= i;
+  }
+  return x;
+}
+
+#elif defined(__APPLE__)
+
+#include <stdlib.h>
+
+#if !(defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101200)
+#error "macOS version not supported (>= 10.12)"
+#endif
+
+unsigned char * __jasmin_syscall_randombytes__(unsigned char *p, uint64_t plen){
+  arc4random_buf(p, plen);
+  return p;
+}
+
+#endif
