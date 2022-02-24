78a3fb8565c21b5cca08fb6eb1afaffc2bc475b0
Tiago Oliveira <tfaoliveira@gmail.com>
2022-02-24 12:19:28 +0100

    fix compilation for clang

--- test/common/notrandombytes.c
+++ test/common/notrandombytes.c
@@ -1,4 +1,3 @@
-// this file is not big endian friendly;
 // adapted from SUPERCOP implementations;
 
 #include <inttypes.h>
@@ -16,7 +15,7 @@
   a = a + b; d = ROTATE((d ^ a), 8); \
   c = c + d; b = ROTATE((b ^ c), 7);
 
-static void chacha20_core(uint8_t output[64], const uint32_t input[16])
+static void chacha20_core(uint32_t output[16], const uint32_t input[16])
 {
   uint32_t x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
   int i;
@@ -66,70 +65,74 @@ static void chacha20_core(uint8_t output[64], const uint32_t input[16])
   x14 += input[14];
   x15 += input[15];
 
-  *((uint32_t*) (output + 0 )) = x0;
-  *((uint32_t*) (output + 4 )) = x1;
-  *((uint32_t*) (output + 8 )) = x2;
-  *((uint32_t*) (output + 12)) = x3;
-  *((uint32_t*) (output + 16)) = x4;
-  *((uint32_t*) (output + 20)) = x5;
-  *((uint32_t*) (output + 24)) = x6;
-  *((uint32_t*) (output + 28)) = x7;
-  *((uint32_t*) (output + 32)) = x8;
-  *((uint32_t*) (output + 36)) = x9;
-  *((uint32_t*) (output + 40)) = x10;
-  *((uint32_t*) (output + 44)) = x11;
-  *((uint32_t*) (output + 48)) = x12;
-  *((uint32_t*) (output + 52)) = x13;
-  *((uint32_t*) (output + 56)) = x14;
-  *((uint32_t*) (output + 60)) = x15;
+  output[0 ] = x0;
+  output[1 ] = x1;
+  output[2 ] = x2;
+  output[3 ] = x3;
+  output[4 ] = x4;
+  output[5 ] = x5;
+  output[6 ] = x6;
+  output[7 ] = x7;
+  output[8 ] = x8;
+  output[9 ] = x9;
+  output[10] = x10;
+  output[11] = x11;
+  output[12] = x12;
+  output[13] = x13;
+  output[14] = x14;
+  output[15] = x15;
 }
 
 static const uint8_t sigma[16] = "expand 32-byte k";
 
 static void chacha20_init(uint32_t x[16], const uint8_t n[8], const uint8_t k[32])
 {
-  x[0]  = *((uint32_t*) (sigma + 0));
-  x[1]  = *((uint32_t*) (sigma + 4));
-  x[2]  = *((uint32_t*) (sigma + 8));
-  x[3]  = *((uint32_t*) (sigma + 12));
+  #define LU8U32(p, i) ((((uint32_t)p[i+3])<<24) | (((uint32_t)p[i+2])<<16) | (((uint32_t)p[i+1])<<8) |(((uint32_t)p[i])<<0))
 
-  x[4]  = *((uint32_t*) (k + 0 ));
-  x[5]  = *((uint32_t*) (k + 4 ));
-  x[6]  = *((uint32_t*) (k + 8 ));
-  x[7]  = *((uint32_t*) (k + 12));
-  x[8]  = *((uint32_t*) (k + 16));
-  x[9]  = *((uint32_t*) (k + 20));
-  x[10] = *((uint32_t*) (k + 24));
-  x[11] = *((uint32_t*) (k + 28));
+  x[0]  = LU8U32(sigma, 0);
+  x[1]  = LU8U32(sigma, 4);
+  x[2]  = LU8U32(sigma, 8);
+  x[3]  = LU8U32(sigma, 12);
+
+  x[4]  = LU8U32(k, 0 );
+  x[5]  = LU8U32(k, 4 );
+  x[6]  = LU8U32(k, 8 );
+  x[7]  = LU8U32(k, 12);
+  x[8]  = LU8U32(k, 16);
+  x[9]  = LU8U32(k, 20);
+  x[10] = LU8U32(k, 24);
+  x[11] = LU8U32(k, 28);
 
   x[12] = 0;
   x[13] = 0;
-  x[14] = *((uint32_t*) (n + 0));
-  x[15] = *((uint32_t*) (n + 4));
+  x[14] = LU8U32(n, 0);
+  x[15] = LU8U32(n, 4);
+
+  #undef LU8U32
 }
 
 
 static void chacha20(uint8_t *c, uint64_t clen, const uint8_t n[8], const uint8_t k[32])
 {
-  uint32_t x[16];
-  uint8_t output[64];
+  uint32_t x[16], output[16];
   uint64_t i;
 
   chacha20_init(x,n,k);
   while(clen > 0)
   {
     chacha20_core(output, x);
-    *((uint64_t*) (x + 12)) += 1;
+    x[12] += 1;
+    if(x[12] == 0){ x[13] += 1; }
 
     if(clen >= 64)
     { for(i=0; i<64; i++)
-      { c[i] = output[i]; }
+      { c[i] = ((uint8_t*)output)[i]; }
       clen -= 64;
       c += 64;
     }
     else
     { for(i=0; i<clen; i++)
-      { c[i] = output[i]; }
+      { c[i] = ((uint8_t*)output)[i]; }
       return;
     }
   }
