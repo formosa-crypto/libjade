425429fd90ab6ebad4757f22776aae6ae59a5122
Peter Schwabe <peter@cryptojedi.org>
2022-03-14 12:43:28 +0100

    More work towards benchmarking

---
+++ ext/randombytes/randombytes.h
@@ -0,0 +1,9 @@
+#ifndef LIBJADE_RANDOMBYTES_H
+#define LIBJADE_RANDOMBYTES_H
+
+#include <stdint.h>
+
+void __jasmin_syscall_randombytes__(uint8_t *x, uint64_t xlen)
+asm("__jasmin_syscall_randombytes__");
+
+#endif
