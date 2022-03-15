08ccb833af7763cf98f8fbd1dbfc6ff25f1cffcb
Peter Schwabe <peter@cryptojedi.org>
2022-03-15 15:17:09 +0100

    ssbd-tools as submodule, some progress on benchmarking

---
+++ benchmark/common/cpucycles.h
@@ -0,0 +1,8 @@
+static inline uint64_t cpucycles(void) {
+  uint64_t result;
+
+  __asm__ volatile ("rdtsc; shlq $32,%%rdx; orq %%rdx,%%rax"
+    : "=a" (result) : : "%rdx");
+
+  return result;
+}
--- ext/randombytes/randombytes.h
+++ ext/randombytes/randombytes.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-void __jasmin_syscall_randombytes__(uint8_t *x, uint64_t xlen)
-asm("__jasmin_syscall_randombytes__");
+void __jasmin_syscall_randombytes__(uint8_t *x, uint64_t xlen);
+//asm("__jasmin_syscall_randombytes__");
 
 #endif
