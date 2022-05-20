a70b07fc036efd69e6921b43fc88a96f31e52b13
Tiago Oliveira <tfaoliveira@gmail.com>
2022-05-20 15:46:09 +0200

    sslh: copy ref* implementations from main (-kyber) and call them mmx

---
+++ src/crypto_stream/chacha/chacha12/amd64/mmx/include/api.h
@@ -0,0 +1,26 @@
+#ifndef JADE_STREAM_CHACHA_CHACHA12_AMD64_REF_API_H
+#define JADE_STREAM_CHACHA_CHACHA12_AMD64_REF_API_H
+
+#define JADE_STREAM_CHACHA_CHACHA12_AMD64_REF_KEYBYTES 32
+#define JADE_STREAM_CHACHA_CHACHA12_AMD64_REF_NONCEBYTES 8
+#define JADE_STREAM_CHACHA_CHACHA12_AMD64_REF_ALGNAME "ChaCha12"
+
+#include <stdint.h>
+
+int jade_stream_chacha_chacha12_amd64_ref_xor(
+ uint8_t *ciphertext,
+ uint8_t *plaintext,
+ uint64_t length,
+ uint8_t *nonce, /*NONCEBYTES*/
+ uint8_t *key /*KEYBYTES*/
+);
+
+int jade_stream_chacha_chacha12_amd64_ref(
+ uint8_t *stream,
+ uint64_t length,
+ uint8_t *nonce, /*NONCEBYTES*/
+ uint8_t *key /*KEYBYTES*/
+);
+
+
+#endif
---
+++ src/crypto_stream/salsa20/salsa2012/amd64/mmx/include/api.h
@@ -0,0 +1,25 @@
+#ifndef JADE_STREAM_SALSA20_SALSA2012_AMD64_REF_API_H
+#define JADE_STREAM_SALSA20_SALSA2012_AMD64_REF_API_H
+
+#define JADE_STREAM_SALSA20_SALSA2012_AMD64_REF_KEYBYTES 32
+#define JADE_STREAM_SALSA20_SALSA2012_AMD64_REF_NONCEBYTES 8
+#define JADE_STREAM_SALSA20_SALSA2012_AMD64_REF_ALGNAME "Salsa20/12"
+
+#include <stdint.h>
+
+int jade_stream_salsa20_salsa2012_amd64_ref_xor(
+ uint8_t *ciphertext,
+ uint8_t *plaintext,
+ uint64_t length,
+ uint8_t *nonce, /*NONCEBYTES*/
+ uint8_t *key /*KEYBYTES*/
+);
+
+int jade_stream_salsa20_salsa2012_amd64_ref(
+ uint8_t *stream,
+ uint64_t length,
+ uint8_t *nonce, /*NONCEBYTES*/
+ uint8_t *key /*KEYBYTES*/
+);
+
+#endif
