bb3c44ccfd508e0c30838f7d876397bdc2fb0159
Tiago Oliveira <tfaoliveira@gmail.com>
2022-07-17 18:10:27 +0100

    crypto_secretbox : xsalsa20poly1305

---
+++ src/crypto_secretbox/xsalsa20poly1305/amd64/avx2/include/api.h
@@ -0,0 +1,28 @@
+#ifndef JADE_SECRETBOX_XSALSA20POLY1305_AMD64_AVX2_API_H
+#define JADE_SECRETBOX_XSALSA20POLY1305_AMD64_AVX2_API_H
+
+#define JADE_SECRETBOX_XSALSA20POLY1305_AMD64_AVX2_KEYBYTES 32
+#define JADE_SECRETBOX_XSALSA20POLY1305_AMD64_AVX2_NONCEBYTES 24
+#define JADE_SECRETBOX_XSALSA20POLY1305_AMD64_AVX2_ZEROBYTES 32
+#define JADE_SECRETBOX_XSALSA20POLY1305_AMD64_AVX2_BOXZEROBYTES 16
+#define JADE_SECRETBOX_XSALSA20POLY1305_AMD64_AVX2_ALGNAME "XSalsa20/20Poly1305"
+
+#include <stdint.h>
+
+int jade_secretbox_xsalsa20poly1305_amd64_avx2(
+  uint8_t *ciphertext,
+  const uint8_t *plaintext,
+  uint64_t length,
+  const uint8_t *nonce, /*NONCEBYTES*/
+  const uint8_t *key /*KEYBYTES*/
+);
+
+int jade_secretbox_xsalsa20poly1305_amd64_avx2_open(
+  uint8_t *plaintext,
+  const uint8_t *ciphertext,
+  uint64_t length,
+  const uint8_t *nonce, /*NONCEBYTES*/
+  const uint8_t *key /*KEYBYTES*/
+);
+
+#endif
