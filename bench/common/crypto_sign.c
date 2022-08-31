a4f912a58bba2330f3bb7dc8c774067d42a44552
Tiago Oliveira <tfaoliveira@gmail.com>
2022-08-31 13:41:35 +0100

    dilithium: wip /

---
+++ bench/common/crypto_sign.c
@@ -0,0 +1,127 @@
+#include "api.h"
+#include "namespace.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//
+
+#define CRYPTO_PUBLICKEYBYTES NAMESPACE(PUBLICKEYBYTES)
+#define CRYPTO_SECRETKEYBYTES NAMESPACE(SECRETKEYBYTES)
+#define CRYPTO_BYTES NAMESPACE(BYTES)
+#define CRYPTO_ALGNAME NAMESPACE(ALGNAME)
+
+#define crypto_sign_keypair NAMESPACE_LC(keypair)
+#define crypto_sign JADE_NAMESPACE_LC
+#define crypto_sign_open NAMESPACE_LC(open)
+
+//
+
+#include "config.h"
+#include "cpucycles.c"
+#include "increment.c"
+
+#define OP 1
+#include "printbench1.c"
+#undef OP
+#define OP 2
+#include "printbench2.c"
+#undef OP
+
+#include "alignedcalloc.c"
+#include "benchrandombytes.c"
+
+//
+
+int main(int argc, char**argv)
+{
+  int loop, r, i;
+  uint64_t cycles[TIMINGS];
+
+  uint64_t  results_keypair[1][LOOPS];
+  uint64_t* results_sign_open[2][LOOPS];
+
+  char *op_str_keypair[]   = { xstr(crypto_sign_keypair,.csv)};
+  char *op_str_sign_open[] = { xstr(crypto_sign,.csv),
+                               xstr(crypto_sign_open,.csv) };
+
+  // TODO smlen needs to be an array
+
+  uint8_t  *_pks,    *pks,    *pk;    // CRYPTO_PUBLICKEYBYTES
+  uint8_t  *_sks,    *sks,    *sk;    // CRYPTO_SECRETKEYBYTES
+  uint8_t  *_ms,     *ms,     *m;     // MAXINBYTES
+  uint8_t  *_sms,    *sms,    *sm;    // CRYPTO_BYTES + MAXINBYTES
+  uint64_t *_smlens, *smlens, *smlen; // sizeof(uint64_t)
+  uint8_t  *_ts,     *ts,     *t;     // MAXINBYTES
+
+  // a_mlen and a_tlen are equal; nonetheless, it improves code readability;
+  size_t a_pklen, a_sklen, a_mlen, a_smlen, a_tlen;
+  size_t mlen, tlen; // [MININBYTES..MAXINBYTES] x2
+
+  //
+
+  pb_alloc_2(results_sign_open, size_inc_in(MININBYTES,MAXINBYTES));
+
+  a_pklen  = alignedcalloc_step(CRYPTO_PUBLICKEYBYTES);
+  a_sklen  = alignedcalloc_step(CRYPTO_SECRETKEYBYTES);
+  a_mlen   = alignedcalloc_step(MAXINBYTES);
+  a_smlen  = alignedcalloc_step(CRYPTO_BYTES + MAXINBYTES);
+  a_tlen   = alignedcalloc_step(MAXINBYTES);
+
+  pks    = alignedcalloc(&_pks, a_pklen * TIMINGS);
+  sks    = alignedcalloc(&_sks, a_sklen * TIMINGS);
+  ms     = alignedcalloc(&_ms,  a_mlen  * TIMINGS);
+  sms    = alignedcalloc(&_sms, a_smlen * TIMINGS);
+  ts     = alignedcalloc(&_ts,  a_tlen  * TIMINGS);
+  smlens = (uint64_t*) alignedcalloc((uint8_t**) &_smlens, sizeof(uint64_t) * TIMINGS);
+
+
+  for(loop = 0; loop < LOOPS; loop++)
+  { 
+    // keypair
+    pk = pks; sk = sks;
+    for (i = 0; i < TIMINGS; i++, pk += a_pklen, sk += a_sklen)
+    { cycles[i] = cpucycles();
+      crypto_sign_keypair(pk, sk); }
+    results_keypair[0][loop] = cpucycles_median(cycles, TIMINGS);
+
+    for (mlen = MININBYTES, r = 0; mlen <= MAXINBYTES; mlen = inc_in(mlen), r += 1)
+    {
+      // for current mlen, initialize TIMINGS messages with mlen bytes
+      m = ms;
+      for (i = 0; i < TIMINGS; i++, m += a_mlen)
+      { benchrandombytes(m, mlen); }
+
+      // sign
+      sm = sms; smlen = smlens; m = ms; sk = sks;
+      for (i = 0; i < TIMINGS; i++, sm += a_smlen, smlen += sizeof(uint64_t), m += a_mlen, sk += a_sklen)
+      { cycles[i] = cpucycles();
+        crypto_sign(sm, smlen, m, mlen, sk); }
+      results_sign_open[0][loop][r] = cpucycles_median(cycles, TIMINGS);
+
+      // open
+      t = ts; sm = sms; smlen = smlens; pk = pks;
+      for (i = 0; i < TIMINGS; i++, t += a_tlen, sm += a_smlen, smlen += sizeof(uint64_t), pk += a_pklen)
+      { cycles[i] = cpucycles();
+        crypto_sign_open(t, &tlen, sm, smlen[0], pk); }
+      results_sign_open[1][loop][r] = cpucycles_median(cycles, TIMINGS);
+    }
+  }
+
+  pb_print_1(argc, results_keypair, op_str_keypair);
+  pb_print_2(argc, results_sign_open, op_str_sign_open);
+
+  pb_free_2(results_sign_open);
+
+  free(_pks);
+  free(_sks);
+  free(_ms);
+  free(_sms);
+  free(_smlens);
+  free(_ts);
+
+  return 0;
+}
+
