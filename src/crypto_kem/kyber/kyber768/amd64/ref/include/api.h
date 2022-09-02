9ea404b7f7a9e8b7c324c45765ea84f805dfa125
MQuaresma <miguelquaresma97@gmail.com>
2022-09-02 15:53:48 +0200

    kyber: import kyber768/ref

--- src/crypto_kem/kyber/kyber768/amd64/ref/include/api.h
+++ src/crypto_kem/kyber/kyber768/amd64/ref/include/api.h
@@ -1,15 +1,18 @@
-#ifndef JADE_KEM_KYBER_KYBER768_AMD64_REF_H
-#define JADE_KEM_KYBER_KYBER768_AMD64_REF_H
+#ifndef JADE_KEM_KYBER_KYBER768_AMD64_REF_API_H
+#define JADE_KEM_KYBER_KYBER768_AMD64_REF_API_H
+
+#include <stdint.h>
 
 #define JADE_KEM_KYBER_KYBER768_AMD64_REF_SECRETKEYBYTES  2400
 #define JADE_KEM_KYBER_KYBER768_AMD64_REF_PUBLICKEYBYTES  1184
 #define JADE_KEM_KYBER_KYBER768_AMD64_REF_CIPHERTEXTBYTES 1088
 #define JADE_KEM_KYBER_KYBER768_AMD64_REF_BYTES           32
+#define JADE_KEM_KYBER_KYBER768_AMD64_REF_ALGNAME         "Kyber768"
 
-int cryptolib_kyber768_amd64_ref_kem_keypair(uint8_t *pk, uint8_t *sk);
+int jade_kem_kyber_kyber768_amd64_ref_keypair(uint8_t *pk, uint8_t *sk);
 
-int cryptolib_kyber768_amd64_ref_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
+int jade_kem_kyber_kyber768_amd64_ref_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
 
-int cryptolib_kyber768_amd64_ref_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
+int jade_kem_kyber_kyber768_amd64_ref_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 
 #endif
