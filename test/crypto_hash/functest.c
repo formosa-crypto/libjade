0e0c2a8c9626b3890364fe76d77e73471de45b45
Tiago Oliveira <tfaoliveira@gmail.com>
2022-11-14 15:59:51 +0100

    crypto_hash functest made simple

--- test/crypto_hash/functest.c
+++ test/crypto_hash/functest.c
@@ -9,36 +9,26 @@
 
 /*
 
-int jade_hash(uint8_t *output, uint8_t *input, uint64_t input_length);
+int jade_hash(
+ uint8_t *hash,
+ const uint8_t *input,
+ uint64_t input_length
+);
 
 */
 
-#define MAXBYTES 32
-
 int main(void)
 {
-  uint8_t output[JADE_HASH_BYTES];
-  uint8_t input[MAXBYTES];
-  uint64_t length = 0;
+  uint8_t hash[JADE_HASH_BYTES];
+  uint8_t input[] = {0x61, 0x62, 0x63};
 
   //
-  memset(output, 0, JADE_HASH_BYTES);
-  memset(input,  0, MAXBYTES);
+  jade_hash(hash, input, sizeof(input));
 
   #ifndef NOPRINT
   print_info(JADE_HASH_ALGNAME, JADE_HASH_ARCH, JADE_HASH_IMPL);
+  print_u8s(hash, JADE_HASH_BYTES);
   #endif
-
-  //
-  for(length=0; length <= MAXBYTES; length++)
-  {
-    jade_hash(output, input, length);
-
-    #ifndef NOPRINT
-    print_str_c_u8("input", length, input, length);
-    print_str_c_u8("output", length, output, JADE_HASH_BYTES);
-    #endif
-  }
 
   return 0;
 }
