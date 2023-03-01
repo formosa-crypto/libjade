7f5772baadf47bb3866f466f87662721f679698d
vincentvbh <vincentvbh7@gmail.com>
2023-03-01 19:00:08 +0100

    add api.h

---
+++ src/crypto_sign/falcon/falcon512/amd64/avx2/include/api.h
@@ -0,0 +1,26 @@
+#ifndef JAD_SIGN_FALCON_FALCON512_AMD64_AVX2_API_H
+#define JAD_SIGN_FALCON_FALCON512_AMD64_AVX2_API_H
+
+#define JADE_SIGN_FALCON_FALCON512_AMD64_AVX2_SECRETKEYBYTES   1281
+#define JADE_SIGN_FALCON_FALCON512_AMD64_AVX2_PUBLICKEYBYTES   897
+#define JADE_SIGN_FALCON_FALCON512_AMD64_AVX2_BYTES            690
+#define JADE_SIGN_FALCON_FALCON512_AMD64_AVX2_ALGNAME          "Falcon-512"
+
+#include <stddef.h>
+
+int 
+jade_sign_falcon_falcon512_amd64_ref_keypair(unsigned char *pk, unsigned char *sk);
+
+int 
+jade_sign_falcon_falcon512_amd64_ref(unsigned char *sm, size_t *smlen,
+    const unsigned char *m, size_t mlen,
+    const unsigned char *sk);
+
+int 
+jade_sign_falcon_falcon512_amd64_avx2_open(unsigned char *m, size_t *mlen,
+    const unsigned char *sm, size_t smlen,
+    const unsigned char *pk);
+
+#endif
+
+
