9ca29c4ab0a789091d4373abde53bf83f126e797
Tiago Oliveira <tfaoliveira@gmail.com>
2023-03-03 13:24:56 +0000

    f512 make + rnd.h

--- test/external/crypto_sign/falcon/falcon512/amd64/avx2/randombytes.h
+++ test/external/crypto_sign/falcon/falcon512/amd64/avx2/randombytes.h
@@ -1,12 +1,8 @@
 #ifndef RANDOMBYTES_H
 #define RANDOMBYTES_H
 
-// replace this with chacha20
-
 #include <stdint.h>
 #include <stddef.h>
-
-typedef uint32_t uint32;
 
 int randombytes(uint8_t *x, size_t xlen);
 
