f610cf701b352e184ad7ed61ef1b213a2cf2e9e2
Peter Schwabe <peter@cryptojedi.org>
2023-04-05 07:14:11 +0200

    Deterministic API now also for kyber768/avx2

--- src/crypto_kem/kyber/kyber768/amd64/avx2/include/api.h
+++ src/crypto_kem/kyber/kyber768/amd64/avx2/include/api.h
@@ -3,18 +3,33 @@
 
 #include <stdint.h>
 
-#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_SECRETKEYBYTES  2400
-#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_PUBLICKEYBYTES  1184
-#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_CIPHERTEXTBYTES 1088
-#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_BYTES           32
+#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_SECRETKEYBYTES    2400
+#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_PUBLICKEYBYTES    1184
+#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_CIPHERTEXTBYTES   1088
+#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2F_KEYPAIRCOINBYTES 64
+#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2F_ENCCOINBYTES     32
+#define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_BYTES             32
 
 #define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_ALGNAME         "Kyber768"
 #define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_ARCH            "amd64"
 #define JADE_KEM_KYBER_KYBER768_AMD64_AVX2_IMPL            "avx2"
 
+int jade_kem_kyber_kyber768_amd64_avx2_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
+
 int jade_kem_kyber_kyber768_amd64_avx2_keypair(
   uint8_t *public_key,
   uint8_t *secret_key
+);
+
+int jade_kem_kyber_kyber768_amd64_avx2_enc_derand(
+  uint8_t *ciphertext,
+  uint8_t *shared_secret,
+  const uint8_t *public_key,
+  const uint8_t *coins
 );
 
 int jade_kem_kyber_kyber768_amd64_avx2_enc(
