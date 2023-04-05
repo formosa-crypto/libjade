539f595cb0dfd1cdaa152d65724a12da08e3b9f3
Peter Schwabe <peter@cryptojedi.org>
2023-04-05 07:37:54 +0200

    Deterministic API also for Kyber512 (ref and avx2)

--- src/crypto_kem/kyber/kyber512/amd64/avx2/include/api.h
+++ src/crypto_kem/kyber/kyber512/amd64/avx2/include/api.h
@@ -3,18 +3,33 @@
 
 #include <stdint.h>
 
-#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_SECRETKEYBYTES  1632
-#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_PUBLICKEYBYTES  800
-#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_CIPHERTEXTBYTES 768
-#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_BYTES           32
+#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_SECRETKEYBYTES   1632
+#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_PUBLICKEYBYTES   800
+#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_CIPHERTEXTBYTES  768
+#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_KEYPAIRCOINBYTES 64
+#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_ENCCOINBYTES     32
+#define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_BYTES            32
 
 #define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_ALGNAME         "Kyber512"
 #define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_ARCH            "amd64"
 #define JADE_KEM_KYBER_KYBER512_AMD64_AVX2_IMPL            "avx2"
 
+int jade_kem_kyber_kyber512_amd64_avx2_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
+
 int jade_kem_kyber_kyber512_amd64_avx2_keypair(
   uint8_t *public_key,
   uint8_t *secret_key
+);
+
+int jade_kem_kyber_kyber512_amd64_avx2_enc_derand(
+  uint8_t *ciphertext,
+  uint8_t *shared_secret,
+  const uint8_t *public_key,
+  const uint8_t *coins
 );
 
 int jade_kem_kyber_kyber512_amd64_avx2_enc(
--- src/crypto_kem/kyber/kyber512/amd64/ref/include/api.h
+++ src/crypto_kem/kyber/kyber512/amd64/ref/include/api.h
@@ -3,19 +3,34 @@
 
 #include <stdint.h>
 
-#define JADE_KEM_KYBER_KYBER512_AMD64_REF_SECRETKEYBYTES  1632
-#define JADE_KEM_KYBER_KYBER512_AMD64_REF_PUBLICKEYBYTES  800
-#define JADE_KEM_KYBER_KYBER512_AMD64_REF_CIPHERTEXTBYTES 768
-#define JADE_KEM_KYBER_KYBER512_AMD64_REF_BYTES           32
+#define JADE_KEM_KYBER_KYBER512_AMD64_REF_SECRETKEYBYTES   1632
+#define JADE_KEM_KYBER_KYBER512_AMD64_REF_PUBLICKEYBYTES   800
+#define JADE_KEM_KYBER_KYBER512_AMD64_REF_CIPHERTEXTBYTES  768
+#define JADE_KEM_KYBER_KYBER512_AMD64_REF_KEYPAIRCOINBYTES 64
+#define JADE_KEM_KYBER_KYBER512_AMD64_REF_ENCCOINBYTES     32
+#define JADE_KEM_KYBER_KYBER512_AMD64_REF_BYTES            32
 
 #define JADE_KEM_KYBER_KYBER512_AMD64_REF_ALGNAME         "Kyber512"
 #define JADE_KEM_KYBER_KYBER512_AMD64_REF_ARCH            "amd64"
 #define JADE_KEM_KYBER_KYBER512_AMD64_REF_IMPL            "ref"
 
 
+int jade_kem_kyber_kyber512_amd64_ref_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
+
 int jade_kem_kyber_kyber512_amd64_ref_keypair(
   uint8_t *public_key,
   uint8_t *secret_key
+);
+
+int jade_kem_kyber_kyber512_amd64_ref_enc_derand(
+  uint8_t *ciphertext,
+  uint8_t *shared_secret,
+  const uint8_t *public_key,
+  const uint8_t *coins
 );
 
 int jade_kem_kyber_kyber512_amd64_ref_enc(
