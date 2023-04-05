878c3961626fcb9b5443b5b1d4b1be16cd58a3f7
Tiago Oliveira <tfaoliveira@gmail.com>
2023-04-05 12:00:23 +0100

    drnd: fix test functest

--- test/common/randombytes.h
+++ test/common/randombytes.h
@@ -1,2 +1,14 @@
 // this is a 'fake' entry point for randombytes.h to test /*/functest.c (which is part of the distribution, which uses ext/*randombytes.h
+
+#ifndef JASMIN_RANDOMBYTES_H
+#define JASMIN_RANDOMBYTES_H
+
+#include <stdint.h>
+
 #include "jasmin_notrandombytes.h"
+
+void randombytes(uint8_t* _x, uint64_t xlen);
+
+#endif
+
+
--- test/include/jade_kem.h
+++ test/include/jade_kem.h
@@ -14,6 +14,9 @@
 #define jade_kem_enc NAMESPACE_LC(enc)
 #define jade_kem_dec NAMESPACE_LC(dec)
 
+#define jade_kem_keypair_derand NAMESPACE_LC(keypair_derand)
+#define jade_kem_enc_derand NAMESPACE_LC(enc_derand)
+
 #define JADE_KEM_ALGNAME NAMESPACE(ALGNAME)
 #define JADE_KEM_ARCH NAMESPACE(ARCH)
 #define JADE_KEM_IMPL NAMESPACE(IMPL)
