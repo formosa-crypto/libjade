fe31539ea5d8f6f90f4b03de806a6d48aa1aa9f6
Peter Schwabe <peter@cryptojedi.org>
2023-04-27 15:27:15 +0200

    Derandomized interface also for Dilithium3

--- src/crypto_sign/dilithium/dilithium3/amd64/avx2/include/api.h
+++ src/crypto_sign/dilithium/dilithium3/amd64/avx2/include/api.h
@@ -1,16 +1,23 @@
 #ifndef JADE_SIGN_dilithium_dilithium3_amd64_avx2_API_H
 #define JADE_SIGN_dilithium_dilithium3_amd64_avx2_API_H
 
-#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_PUBLICKEYBYTES  1952
-#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_SECRETKEYBYTES  4016
-#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_BYTES           3293
-#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_DETERMINISTIC   1
+#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_PUBLICKEYBYTES   1952
+#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_SECRETKEYBYTES   4016
+#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_BYTES            3293
+#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_KEYPAIRCOINBYTES 32
+#define JADE_SIGN_dilithium_dilithium3_amd64_avx2_DETERMINISTIC    1
 
 #define JADE_SIGN_dilithium_dilithium3_amd64_avx2_ALGNAME         "Dilithium3"
 #define JADE_SIGN_dilithium_dilithium3_amd64_avx2_ARCH            "amd64"
 #define JADE_SIGN_dilithium_dilithium3_amd64_avx2_IMPL            "avx2"
 
 #include <stdint.h>
+
+int jade_sign_dilithium_dilithium3_amd64_avx2_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
 
 int jade_sign_dilithium_dilithium3_amd64_avx2_keypair(
   uint8_t *public_key,
--- src/crypto_sign/dilithium/dilithium3/amd64/ref/include/api.h
+++ src/crypto_sign/dilithium/dilithium3/amd64/ref/include/api.h
@@ -1,16 +1,23 @@
 #ifndef JADE_SIGN_dilithium_dilithium3_amd64_ref_API_H
 #define JADE_SIGN_dilithium_dilithium3_amd64_ref_API_H
 
-#define JADE_SIGN_dilithium_dilithium3_amd64_ref_PUBLICKEYBYTES  1952
-#define JADE_SIGN_dilithium_dilithium3_amd64_ref_SECRETKEYBYTES  4016
-#define JADE_SIGN_dilithium_dilithium3_amd64_ref_BYTES           3293
-#define JADE_SIGN_dilithium_dilithium3_amd64_ref_DETERMINISTIC   1
+#define JADE_SIGN_dilithium_dilithium3_amd64_ref_PUBLICKEYBYTES   1952
+#define JADE_SIGN_dilithium_dilithium3_amd64_ref_SECRETKEYBYTES   4016
+#define JADE_SIGN_dilithium_dilithium3_amd64_ref_BYTES            3293
+#define JADE_SIGN_dilithium_dilithium3_amd64_ref_KEYPAIRCOINBYTES 32
+#define JADE_SIGN_dilithium_dilithium3_amd64_ref_DETERMINISTIC    1
 
 #define JADE_SIGN_dilithium_dilithium3_amd64_ref_ALGNAME         "Dilithium3"
 #define JADE_SIGN_dilithium_dilithium3_amd64_ref_ARCH            "amd64"
 #define JADE_SIGN_dilithium_dilithium3_amd64_ref_IMPL            "ref"
 
 #include <stdint.h>
+
+int jade_sign_dilithium_dilithium3_amd64_ref_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
 
 int jade_sign_dilithium_dilithium3_amd64_ref_keypair(
   uint8_t *public_key,
