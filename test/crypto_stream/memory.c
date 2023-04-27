e7ef3b802cc7e720a131f2d5d50fdec7a96739a7
Tiago Oliveira <tfaoliveira@gmail.com>
2023-04-27 19:07:08 +0100

    val: add jade_stream memory

---
+++ test/crypto_stream/memory.c
@@ -0,0 +1,64 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "api.h"
+#include "namespace.h"
+#include "jade_stream.h"
+#include "randombytes.h"
+#include "config.h"
+
+/*
+int jade_stream_xor(
+ uint8_t *output,
+ const uint8_t *input,
+ uint64_t input_length,
+ const uint8_t *nonce,
+ const uint8_t *key
+);
+
+int jade_stream(
+ uint8_t *stream,
+ uint64_t stream_length,
+ const uint8_t *nonce,
+ const uint8_t *key
+);
+*/
+
+int main(void)
+{
+  uint8_t *output;
+  uint8_t *input;
+  uint64_t input_length;
+  uint8_t *nonce;
+  uint8_t *key;
+
+  nonce = malloc(sizeof(uint8_t) * JADE_STREAM_NONCEBYTES);
+  key = malloc(sizeof(uint8_t) * JADE_STREAM_KEYBYTES);
+
+  __jasmin_syscall_randombytes__(nonce, JADE_STREAM_NONCEBYTES);
+  __jasmin_syscall_randombytes__(key, JADE_STREAM_KEYBYTES);
+
+
+  for (input_length = MININBYTES; input_length <= MAXINBYTES; input_length++)
+  {
+    input = malloc(sizeof(uint8_t) * input_length);
+    memset(input, 0, sizeof(uint8_t) * input_length);
+    output = malloc(sizeof(uint8_t) * input_length);
+
+    jade_stream_xor(output, input, input_length, nonce, key);
+    jade_stream(input, input_length, nonce, key);
+
+    free(input);
+    free(output);
+    input = NULL;
+    output = NULL;
+  }
+
+  free(nonce);
+  free(key);
+
+  return 0;
+}
+
