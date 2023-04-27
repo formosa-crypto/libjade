882f119a9316369e2bd0b2b5993eb77f8104746c
Peter Schwabe <peter@cryptojedi.org>
2023-04-27 14:55:14 +0200

    Deterministic API for Dilithium2, AMD64, ref

--- src/crypto_sign/dilithium/dilithium2/amd64/ref/include/api.h
+++ src/crypto_sign/dilithium/dilithium2/amd64/ref/include/api.h
@@ -1,16 +1,23 @@
 #ifndef JADE_SIGN_dilithium_dilithium2_amd64_ref_API_H
 #define JADE_SIGN_dilithium_dilithium2_amd64_ref_API_H
 
-#define JADE_SIGN_dilithium_dilithium2_amd64_ref_PUBLICKEYBYTES  1312
-#define JADE_SIGN_dilithium_dilithium2_amd64_ref_SECRETKEYBYTES  2528
-#define JADE_SIGN_dilithium_dilithium2_amd64_ref_BYTES           2420
-#define JADE_SIGN_dilithium_dilithium2_amd64_ref_DETERMINISTIC   1
+#define JADE_SIGN_dilithium_dilithium2_amd64_ref_PUBLICKEYBYTES   1312
+#define JADE_SIGN_dilithium_dilithium2_amd64_ref_SECRETKEYBYTES   2528
+#define JADE_SIGN_dilithium_dilithium2_amd64_ref_BYTES            2420
+#define JADE_SIGN_dilithium_dilithium2_amd64_ref_KEYPAIRCOINBYTES 32
+#define JADE_SIGN_dilithium_dilithium2_amd64_ref_DETERMINISTIC    1
 
 #define JADE_SIGN_dilithium_dilithium2_amd64_ref_ALGNAME         "Dilithium2"
 #define JADE_SIGN_dilithium_dilithium2_amd64_ref_ARCH            "amd64"
 #define JADE_SIGN_dilithium_dilithium2_amd64_ref_IMPL            "ref"
 
 #include <stdint.h>
+
+int jade_sign_dilithium_dilithium2_amd64_ref_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
 
 int jade_sign_dilithium_dilithium2_amd64_ref_keypair(
   uint8_t *public_key,
