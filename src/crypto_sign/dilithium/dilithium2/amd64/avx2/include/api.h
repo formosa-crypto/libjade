82c66bdd0ab3c1d806b92abecdafb2cdba855700
Peter Schwabe <peter@cryptojedi.org>
2023-04-27 15:06:35 +0200

    Derandomized Dilithium2 avx2

--- src/crypto_sign/dilithium/dilithium2/amd64/avx2/include/api.h
+++ src/crypto_sign/dilithium/dilithium2/amd64/avx2/include/api.h
@@ -1,16 +1,23 @@
 #ifndef JADE_SIGN_dilithium_dilithium2_amd64_avx2_API_H
 #define JADE_SIGN_dilithium_dilithium2_amd64_avx2_API_H
 
-#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_PUBLICKEYBYTES  1312
-#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_SECRETKEYBYTES  2528
-#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_BYTES           2420
-#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_DETERMINISTIC   1
+#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_PUBLICKEYBYTES   1312
+#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_SECRETKEYBYTES   2528
+#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_BYTES            2420
+#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_KEYPAIRCOINBYTES 32
+#define JADE_SIGN_dilithium_dilithium2_amd64_avx2_DETERMINISTIC    1
 
 #define JADE_SIGN_dilithium_dilithium2_amd64_avx2_ALGNAME         "Dilithium2"
 #define JADE_SIGN_dilithium_dilithium2_amd64_avx2_ARCH            "amd64"
 #define JADE_SIGN_dilithium_dilithium2_amd64_avx2_IMPL            "avx2"
 
 #include <stdint.h>
+
+int jade_sign_dilithium_dilithium2_amd64_avx2_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
 
 int jade_sign_dilithium_dilithium2_amd64_avx2_keypair(
   uint8_t *public_key,
