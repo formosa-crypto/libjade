679eea3e33fc1890c1e82fb32fd1042ab43b6653
Pravek Sharma <sharmapravek@gmail.com>
2023-09-27 17:58:59 +0200

    Add NIST KAT testing from liboqs

--- test/crypto_kem/nistkat.c
+++ test/crypto_kem/nistkat.c
@@ -1,3 +1,8 @@
+// Taken from liboqs tests/kat_kem.c and modified for libjade by Pravek Sharma
+// This KAT test only generates a subset of the NIST KAT files.
+// To extract the subset from a submission file, use the command:
+//     cat PQCkemKAT_whatever.rsp | head -n 8 | tail -n 6
+
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -7,42 +12,20 @@
 #include "api.h"
 #include "namespace.h"
 #include "jade_kem.h"
-#include "randombytes.h"
+#include "nistkatrng.h"
 #include "config.h"
 
-/*
-int jade_kem_keypair(
-  uint8_t *public_key,
-  uint8_t *secret_key
-);
-
-int jade_kem_enc(
-  uint8_t *ciphertext,
-  uint8_t *shared_secret,
-  const uint8_t *public_key
-);
-
-int jade_kem_dec(
-  uint8_t *shared_secret,
-  const uint8_t *ciphertext,
-  const uint8_t *secret_key
-);
-
-//
-
-int jade_kem_keypair_derand(
-  uint8_t *public_key,
-  uint8_t *secret_key,
-  const uint8_t *coins
-);
-
-int jade_kem_enc_derand(
-  uint8_t *ciphertext,
-  uint8_t *shared_secret,
-  const uint8_t *public_key,
-  const uint8_t *coins
-);
-*/
+static void fprintBstr(FILE *fp, const char *S, const uint8_t *A, size_t L) {
+	size_t i;
+	fprintf(fp, "%s", S);
+	for (i = 0; i < L; i++) {
+		fprintf(fp, "%02X", A[i]);
+	}
+	if (L == 0) {
+		fprintf(fp, "00");
+	}
+	fprintf(fp, "\n");
+}
 
 int main(void)
 {
@@ -50,25 +33,50 @@ int main(void)
   uint8_t public_key[JADE_KEM_PUBLICKEYBYTES];
   uint8_t secret_key[JADE_KEM_SECRETKEYBYTES];
 
-  uint8_t shared_secret[JADE_KEM_BYTES];
+  uint8_t shared_secret_e[JADE_KEM_BYTES];
+  uint8_t shared_secret_d[JADE_KEM_BYTES];
   uint8_t ciphertext[JADE_KEM_CIPHERTEXTBYTES];
 
   uint8_t keypair_coins[JADE_KEM_KEYPAIRCOINBYTES];
   uint8_t enc_coins[JADE_KEM_ENCCOINBYTES];
 
-  __jasmin_syscall_randombytes__(keypair_coins, JADE_KEM_KEYPAIRCOINBYTES);
+	uint8_t entropy_input[48];
+	uint8_t seed[48];
+
+	FILE *fh = NULL;
+
+  for (uint8_t i = 0; i < 48; i++) {
+		entropy_input[i] = i;
+	}
+
+  nist_kat_init(entropy_input, NULL, 256);
+
+	fh = stdout;
+
+  fprintf(fh, "count = 0\n");
+  nist_kat(seed, 48);
+	fprintBstr(fh, "seed = ", seed, 48);
+
+  nist_kat_init(seed, NULL, 256);
+
+  nist_kat(keypair_coins, JADE_KEM_KEYPAIRCOINBYTES/2);
+  nist_kat((uint8_t *)(keypair_coins + (JADE_KEM_KEYPAIRCOINBYTES/2)), JADE_KEM_KEYPAIRCOINBYTES/2);
   r = jade_kem_keypair_derand(public_key, secret_key, keypair_coins);
     assert(r == 0);
+	fprintBstr(fh, "pk = ", public_key, JADE_KEM_PUBLICKEYBYTES);
+	fprintBstr(fh, "sk = ", secret_key, JADE_KEM_SECRETKEYBYTES);
 
-  __jasmin_syscall_randombytes__(enc_coins, JADE_KEM_ENCCOINBYTES);
-  r = jade_kem_enc_derand(ciphertext, shared_secret, public_key, enc_coins);
+  nist_kat(enc_coins, JADE_KEM_ENCCOINBYTES);
+  r = jade_kem_enc_derand(ciphertext, shared_secret_e, public_key, enc_coins);
+    assert(r == 0);
+	fprintBstr(fh, "ct = ", ciphertext, JADE_KEM_CIPHERTEXTBYTES);
+	fprintBstr(fh, "ss = ", shared_secret_e, JADE_KEM_BYTES);
+
+  r = jade_kem_dec(shared_secret_d, ciphertext, secret_key);
     assert(r == 0);
 
-  r = jade_kem_dec(shared_secret, ciphertext, secret_key);
+	r = memcmp(shared_secret_e, shared_secret_d, JADE_KEM_BYTES);
     assert(r == 0);
-
-  // TODO: some hash, to be replaced by the actual checksum
-  printf("5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03");
 
   return 0;
 }
