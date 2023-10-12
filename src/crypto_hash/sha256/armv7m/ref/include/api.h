afaa6d3640d963e670b68910f3da06c1d53fa017
Tiago Oliveira <tfaoliveira@gmail.com>
2023-10-12 19:57:56 +0100

    armv7m: sha256 ref

---
+++ src/crypto_hash/sha256/armv7m/ref/include/api.h
@@ -0,0 +1,18 @@
+#ifndef JADE_HASH_sha256_armv7m_ref_API_H
+#define JADE_HASH_sha256_armv7m_ref_API_H
+
+#define JADE_HASH_sha256_armv7m_ref_BYTES 32
+
+#define JADE_HASH_sha256_armv7m_ref_ALGNAME "SHA256"
+#define JADE_HASH_sha256_armv7m_ref_ARCH    "armv7m"
+#define JADE_HASH_sha256_armv7m_ref_IMPL    "ref"
+
+#include <stdint.h>
+
+int jade_hash_sha256_armv7m_ref(
+ uint8_t *hash,
+ const uint8_t *input,
+ uint32_t input_length
+);
+
+#endif
