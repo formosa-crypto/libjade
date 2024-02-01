7c2e6aa6921065a38f07542fb967d965a6dcad31
Joao Diogo Duarte <joao@diogoduarte.pt>
2024-02-01 17:57:21 +0000

    X-Wing implemented but either enc or dec does not work properly

--- test/include/jade_kem.h
+++ test/include/jade_kem.h
@@ -17,13 +17,6 @@
 #define jade_kem_keypair_derand NAMESPACE_LC(keypair_derand)
 #define jade_kem_enc_derand NAMESPACE_LC(enc_derand)
 
-#define jade_xkem_keypair NAMESPACE_LC(keypair)
-#define jade_xkem_enc NAMESPACE_LC(enc)
-#define jade_xkem_dec NAMESPACE_LC(dec)
-
-#define jade_xkem_keypair_derand NAMESPACE_LC(keypair_derand)
-#define jade_xkem_enc_derand NAMESPACE_LC(enc_derand)
-
 #define JADE_KEM_ALGNAME NAMESPACE(ALGNAME)
 #define JADE_KEM_ARCH NAMESPACE(ARCH)
 #define JADE_KEM_IMPL NAMESPACE(IMPL)
