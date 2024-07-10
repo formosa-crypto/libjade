a42d4e095171d17e2a9ee7564e10832b68395951
Tiago Oliveira <tfaoliveira@gmail.com>
2024-07-10 15:07:05 +0100

    update the Jasmin compiler 2

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "nixpkgs/release-23.11";
     easycrypt.url = "github:EasyCrypt/easycrypt/4201fddc14b81d2a69a33f034c9c7db4dfd58d0e";
     jasmin = {
-      url = "github:jasmin-lang/jasmin/e84c0c59b4f4e005f2be4de5fdfbcaf1e3e2f975";
+      url = "github:jasmin-lang/jasmin/e4640e7dcdb01d1ba63617a5d78456e1209d699c";
       flake = false;
     };
   };
