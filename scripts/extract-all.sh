57f254596032e4cb9df996d049c9fa2b056ee33f
Peter Schwabe <peter@cryptojedi.org>
2024-08-07 16:52:59 +0200

    Updated extract-all script

--- scripts/extract-all.sh
+++ scripts/extract-all.sh
@@ -2,10 +2,21 @@
 
 SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LIBJADE_ROOT="$SCRIPTDIR/.."
+LIBJADE_SRC="$LIBJADE_ROOT/src"
+
+rm -r "$LIBJADE_SRC"
 
 for SUBMODULE in "$LIBJADE_ROOT"/submodules/formosa-*;do
   EXTRACT="$SUBMODULE/extract-libjade.sh"
   for IMPLEMENTATION in $($EXTRACT --list-implementations); do
-    $EXTRACT --gen-implementation "$IMPLEMENTATION" "$LIBJADE_ROOT/src/$IMPLEMENTATION"
+
+    IMPLPATH="$LIBJADE_SRC/$IMPLEMENTATION"
+
+    if [ -e "$IMPLPATH" ];then
+      echo "Error: $IMPLPATH already exists"
+    else
+      mkdir -p "$IMPLPATH"
+      $EXTRACT --gen-implementation "$IMPLEMENTATION" "$IMPLPATH"
+    fi
   done
 done
